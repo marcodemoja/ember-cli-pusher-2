ddb2cf4ea978f752051792726c2174aa239a0acb
marco de moja <marco@marcos-iMac.homenet.telecomitalia.it>
2016-01-31 17:33:14 +0100

    il 2 cazzo il 2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
     this._super.included(app);
 
     this.app.import(app.bowerDirectory + '/pusher/dist/pusher.js');
-    this.app.import(app.bowerDirectory + '/ember-pusher-2/ember-pusher.amd.js', {
+    this.app.import(app.bowerDirectory + '/ember-pusher-2/ember-pusher-2.amd.js', {
       exports: {
         'ember-pusher-2/controller':    ['Controller'],
         'ember-pusher-2/bindings':      ['Bindings'],
